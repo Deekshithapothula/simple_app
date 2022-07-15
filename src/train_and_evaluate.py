dd16793dbe0cf605d0c965e379afea89da9c2c34
Deekshitha <pothuladeekshitha11@gmail.com>
2022-07-16 02:03:07 +0530

    tracker updated

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -21,6 +21,7 @@ def eval_metrics(actual,pred):
     rmse=np.sqrt(mean_squared_error(actual,pred))
     mae=mean_absolute_error(actual, pred)
     r2=r2_score(actual,pred)
+    return rmse,mae,r2
 
 def train_and_evaluate(config_path):
     config=read_params(config_path)
@@ -50,6 +51,32 @@ def train_and_evaluate(config_path):
 
     predicted_qualities=lr.predict(test_x)
     (rmse,mae,r2)=eval_metrics(test_y,predicted_qualities)
+
+    print("ElasticNet model (alpha=%f,l1_ratio=%f):" %(alpha,l1_ratio))
+    print(" RMSE: %s" %rmse)
+    print(" mae: %s" %mae)
+    print(" R2: %s" %r2)
+
+
+    ####################################################
+
+
+    scores_file=config["reports"]["scores"]
+    params_file=config["reports"]["params"]
+
+    with open(scores_file,"w") as f:
+        scores={"rmse":rmse,"mae":mae,"r2":r2}
+        json.dump(scores,f,indent=4)
+    
+    with open(params_file,"w") as f:
+        params ={"alpha":alpha,"l1_ratio":l1_ratio}
+        json.dump(params,f,indent=4)
+
+    ###########################################################
+    os.makedirs(model_dir, exist_ok= True)
+    model_path =os.path.join(model_dir,"model.joblib")
+
+    joblib.dump(lr,model_path)
 
 if __name__ =="__main__":
     args =argparse.ArgumentParser()
